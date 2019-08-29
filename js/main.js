b4a5277251d8c21f3dbf600f98850ce61a3c1d73
azhar <azharulislamnipu@gmail.com>
2019-08-30 02:19:04 +0600

    Home Page Completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 (function($) {
     "use strict";
+    var $win = $(window);
+    var $doc = $(document);
+    var $body = $('body');
     
     /*--
-    Menu Stick
+    sticky header
     -----------------------------------*/
+    $win.scroll(function() {
+        if ($(this).scrollTop() > 130){
+            $('header').addClass("sticky");
+        }
+        else{
+            $('header').removeClass("sticky");
+        }
+    });
    
     
     
