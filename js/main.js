93029aba5a1abf7feaf41fa637adf81a0766c3cd
azhar <azharulislamnipu@gmail.com>
2019-09-01 03:00:53 +0600

    latest Updated

--- js/main.js
+++ js/main.js
@@ -15,8 +15,19 @@
             $('header').removeClass("sticky");
         }
     });
-   
-    
-    
+
+    /*--
+        isotope
+        -----------------------------------*/
+    if ($('.breed-list').length>0){
+        var $grid = $('.breed-list').isotope({
+            itemSelector: '.breed-item',
+        });
+        $('.search-filter').on( 'click', 'li>a', function() {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+    }
+
     
 })(jQuery);
